e0523d472cd75fb5c09ab9d30e940e09a68adeaf
Alar-q <alar.akilbekov@gmail.com>
2024-10-06 18:01:04 +0500

    none

--- src/download_playlist.py
+++ src/download_playlist.py
@@ -2,7 +2,8 @@ from playlister import playlist_links
 from videos_downloader import download_videos
 from titles_normalizer import normalize
 
-playlists = ['https://www.youtube.com/playlist?list=PLZHQObOWTQDPD3MizzM2xVFitgF8hE_ab']
+playlists = ['https://www.youtube.com/playlist?list=PL6-BrcpR2C5Q1ivGTQcglILJG6odT2oCY', 'https://www.youtube.com/playlist?list=PL6-BrcpR2C5RYoCAmC8VQp_rxSh0i_6C6']
+# ['https://www.youtube.com/playlist?list=PLZHQObOWTQDPD3MizzM2xVFitgF8hE_ab']
 
 for playlist in playlists:
     title, links = playlist_links(playlist_url=playlist)
